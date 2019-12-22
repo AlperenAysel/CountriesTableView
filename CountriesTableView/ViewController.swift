b1e15f48fb5722e0eb86852ef96e9d56c5baf6db
AlperenAysel <48491546+AlperenAysel@users.noreply.github.com>
2019-12-22 16:05:47 +0300

    Initial Commit

---
+++ CountriesTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountriesTableView
+//
+//  Created by Alperen Aysel on 22.12.2019.
+//  Copyright © 2019 Alperen Aysel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
